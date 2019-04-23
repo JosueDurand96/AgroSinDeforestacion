4089726d854eec7ea043bbdd6365bc113a7841ef
JosueDurand96 <josue.durand8@gmail.com>
2019-04-23 16:49:34 -0500

    login

---
+++ app/src/main/java/agrosindeforestacion/com/agrosindeforestacion/LoginActivity.java
@@ -0,0 +1,22 @@
+package agrosindeforestacion.com.agrosindeforestacion;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.escenario, new SessionFragment()).commit();
+
+    }
+
+
+}
